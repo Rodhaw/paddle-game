afc51ac3e0f383f0ad18744d0a2300eae95eccf9
Rodrigo Zarate <rozache_3@hotmail.com>
2023-05-05 08:42:52 -0600

    Rebound fixes

---
+++ src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<header>
+    <h1>Padel Game</h1>
+    <h2>Have old timer fun!</h2>
+</header>
+
+<style>
+    h1, h2{
+        margin: 0;
+    }
+
+    h1{
+        margin-bottom: 1rem;
+    }
+</style>
